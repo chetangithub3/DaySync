8dd57dd79d763aca6bcd045b1c3799741d82b787
Chetan Dhowlaghar <chetan.getsmail@gmail.com>
2023-09-16 20:32:27 -0400

    Project folders categorized

---
+++ DaySync/Constants/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  DaySync
+//
+//  Created by Chetan Dhowlaghar on 9/16/23.
+//
+
+import Foundation
+
+struct Constants {
+    
+    enum API: String {
+        case baseURL = "https://api.sunrise-sunset.org/json"
+        
+    }
+    
+}
---
+++ DaySync/Utility/Utility.swift
@@ -0,0 +1,8 @@
+//
+//  Utility.swift
+//  DaySync
+//
+//  Created by Chetan Dhowlaghar on 9/16/23.
+//
+
+import Foundation
