3cde9a00dc90f1d22937bece8f15a36b6a93d4a2
Chetan Dhowlaghar <chetan.getsmail@gmail.com>
2023-09-16 20:13:19 -0400

    Initial Commit

---
+++ DaySync/DaySyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaySyncApp.swift
+//  DaySync
+//
+//  Created by Chetan Dhowlaghar on 9/16/23.
+//
+
+import SwiftUI
+
+@main
+struct DaySyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
