4f08c7c0175d47756d527f0ab172a4000b88b060
Chetan Dhowlaghar <chetan.getsmail@gmail.com>
2023-09-17 12:36:36 -0400

    added fetch function

--- DaySync/View Models/DashboardViewModel.swift
+++ DaySync/View Models/DashboardViewModel.swift
@@ -6,7 +6,32 @@
 //
 
 import Foundation
+import Combine
 
 class DashboardViewModel: ObservableObject {
+    
+    @Published var sunrise = "Hello selpuu"
+    
+    func fetchData(from url: URL) {
+        APIManager.publisher(for: url)
+            .sink (receiveCompletion: { (completion) in
+                switch completion {
+                    case .finished:
+                        return
+                    case .failure(let error):
+                        print(error.localizedDescription)
+                       
+                        
+                }
+            }, receiveValue: { (daylightData: DaylightData) in
+                
+
+                
+                
+                
+            })
+            
+    }
+    
     
 }
